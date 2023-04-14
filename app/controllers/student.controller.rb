515432585a4f16a610494a3542fce2aa137cacf0
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-14 16:18:16 +0300

    created student controller

---
+++ app/controllers/student.controller.rb
@@ -0,0 +1,16 @@
+class StudentController < ApplicationController
+    post '/students' do
+        student = Student.create(name: params[:name], grade_id: params[:grade_id])
+        student.to_json
+    end
+    get '/students' do
+        student = Student.all
+        student.to_json
+    end
+
+    delete '/students' do
+        student = Student.find(params[:id])
+        student.destroy
+        student.to_json
+    end
+end
