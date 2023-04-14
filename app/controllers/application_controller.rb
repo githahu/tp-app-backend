f238e14ba8d2c0f5329df29ac40ab7f21d529128
Edwin Mwangi Njoroge <edwin.njoroge@student.moringaschool.com>
2023-04-14 16:20:00 +0300

    modified application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
+  # allow access-control-allow-origin header on all requests
+  before do
+    response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
+# enable CORS preflight requests
+options "*" do
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+end
   # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
